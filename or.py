a36b7bcd024c475ca86c0a4f2908e020f2ea6a09
Manirathinam21 <Manirathinam21@gmail.com>
2021-09-15 17:36:05 +0530

    last

--- or.py
+++ or.py
@@ -3,25 +3,34 @@ from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
+
+def main(data, eta, epochs, filename, plotFileName):
+    
   
-df = pd.DataFrame(OR)
+    df = pd.DataFrame(data)
 
-print(df)
+    print(df)
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    _ = model.total_loss()
 
-_ = model.total_loss()
+    save_model(model, filename=filename)
+    save_plot(df, plotFileName, model)
 
-save_model(model, filename="or.model")
-save_plot(df, "or.png", model)
+if __name__== '__main__':  # << entry point 
+    
+    OR = {
+       "x1": [0,0,1,1],
+       "x2": [0,1,0,1],
+       "y": [0,1,1,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=OR, eta=ETA, epochs=EPOCHS, filename="or.model",plotFileName="or.png") 
